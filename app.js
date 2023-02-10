bfe3572658ca98b17dd1045562ded36a9d10d3fa
Matheus Gomes <matheusoutros@hotmail.com>
2023-02-10 01:10:37 -0300

    Finalizando o projeto individual

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ var prompt = require('prompt-sync')({
     history: require('prompt-sync-history')() //open history file
   });
 
-let question = prompt('Bem vindo ao armazém de propriedades do CSS, quer armazenar? (s/n) ');
 
-console.log('Você escreveu (' + question + '), escreva apenas (s/n), agora aperte CTRL + C para voltar a pergunta!')
-
+let question = prompt('Bem vindo ao armazém de propriedades do CSS, quer armazenar? (s/n) ')
 const armazem = [];
 
 if(question == 's'||'n'){
+    
     while(question != "n"){
+        
         if(question == "s"){
+            
             var propriedadeCSS = prompt('Qual é a propriedade do CSS? ')
             
             if(propriedadeCSS == "SAIR"){
@@ -19,11 +20,14 @@ if(question == 's'||'n'){
             armazem.push(propriedadeCSS)
             armazem.sort()
             console.log(armazem)
+            
         }
         
     }
-        
-} else{
+}else{
     console.log('Você escreveu (' + question + '), escreva apenas (s/n), agora aperte CTRL + C para voltar a pergunta!')
 }
+
+
+// armazem.history.save()
 
